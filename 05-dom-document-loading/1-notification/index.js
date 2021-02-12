ba6f424f983b3d8bedd737e260960228e028919b
Serhii7 <sergiyp777@gmail.com>
2021-02-12 03:50:52 +0200

    Работа с DOM. Загрузка документа

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+    static message = null;
+    static timeout = null;
 
+    constructor(message = '', {
+        duration = 2000,
+        type = 'success'
+    } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+
+        this.show();
+    }
+
+    get template() {
+        return `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">${this.message}</div>
+            </div>
+        </div>`;
+    }
+
+    show(container = '') {
+        this.destroy();
+        this.remove();
+
+        const element = container || document.createElement('div');
+
+        element.innerHTML = this.template;
+        NotificationMessage.message = element.firstElementChild;
+
+        this.element = element.firstElementChild;
+
+        NotificationMessage.timeout = setTimeout(this.destroy, this.duration);
+    }
+
+    remove() {
+        this.element && this.element.remove();
+    }
+
+    destroy() {
+        if (NotificationMessage.message) {
+            NotificationMessage.message.remove();
+            NotificationMessage.message = null;
+            clearTimeout(NotificationMessage.timeout);
+        }
+    }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,101 @@
 export default class SortableTable {
+    subElements = {};
 
+    constructor(header = [], {data} = []) {
+        this.header = header;
+        this.data = data;
+
+        this.render();
+    }
+
+    sort(field = 'title', param = 'asc') {
+        let method;
+
+        switch (param) {
+            case 'asc':
+                method = 1;
+                break;
+            case 'desc':
+                method = -1;
+                break;
+            default:
+                return;
+        }
+
+        this.data = this.data.sort((str1, str2) => {
+            if (isNaN(str1[field]) && isNaN(str2[field])) {
+                return str1[field].localeCompare(str2[field], ['ru', 'en'], {caseFirst: 'upper'}) * method;
+            } else {
+                return (str1[field] - str2[field]) * method;
+            }
+        });
+
+        this.subElements.body ? this.subElements.body.innerHTML = this.templateBody : '';
+    }
+
+
+    render() {
+        const element = document.createElement('div');
+
+        element.innerHTML = `<div data-element="productsContainer" class="products-list__container">
+            <div class="sortable-table">
+                <div data-element="header" class="sortable-table__header sortable-table__row">
+                    ${this.templateHeader}
+                </div>
+                <div data-element="body" class="sortable-table__body">
+                    ${this.templateBody}
+                </div>
+            </div>
+        </div>`;
+
+        this.element = element.firstElementChild;
+        this.subElements = this.getSubElements(this.element);
+    }
+
+    getSubElements(element) {
+        const elements = element.querySelectorAll('[data-element]');
+
+        return [...elements].reduce((obj, subElement) => {
+          obj[subElement.dataset.element] = subElement;
+
+          return obj;
+        }, {});
+    }
+
+    get templateHeader() {
+        return this.header.map(item => {
+            return `<div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}">
+                <span>${item.title}</span>
+            </div>`;
+        }).join('');
+    }
+
+    get templateBody() {
+        return this.data.map(item => {
+            const image = item.images && `<div class="sortable-table__cell">
+                <img class="sortable-table-image" alt="Image" src="${item.images}">
+            </div>`;
+            const quantity = item.quantity && `<div class="sortable-table__cell">${item.quantity}</div>`;
+            const sales = item.sales && `<div class="sortable-table__cell">${item.sales}</div>`;
+
+            return `<a href="/products/${item.id}" class="sortable-table__row">
+                ${image || ''}
+                <div class="sortable-table__cell">${item.title}</div>
+                ${quantity || ''}
+                <div class="sortable-table__cell">${item.price}</div>
+                ${sales || ''}
+             </a>`;
+        }).join('');
+    }
+
+    remove() {
+        this.element && this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        this.element = null;
+        this.subElement = {}
+    }
 }
 
